d5e7ff1403df045e65369acf68642eb8ec2ea2e4
BondPV <bondpv@yandex.ru>
2023-03-04 18:29:29 +0300

    feat: add dotenv

--- config/default.ts
+++ config/default.ts
@@ -1,20 +1,29 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+const DB: string = process.env.DB_URI || 'url mongodb database';
+const TOKEN_SECRET_KEY: string = process.env.SECRET_KEY || '';
+const TELEGRAM_TOKEN: string = process.env.TELEGRAM_TOKEN || '';
+const TELEGRAM_CHAT: string = process.env.TELEGRAM_CHAT || '';
+
+
 export default {
   port: 3005,
   host: 'localhost',
-  dbUri: 'mongodb+srv://bondpv:mongo2023bpv@cluster0.cbz4yfq.mongodb.net/MAPmoney?retryWrites=true&w=majority',
+  dbUri: DB,
 };
 
 export const bcryptSaltRounds = 10;
 
 export const tokenSettings = {
-  secretKey: 'MAPMoney',
+  secretKey: TOKEN_SECRET_KEY,
   time: '30d',
 };
 
 export const telegram = {
   url: 'https://api.telegram.org',
-  token: '6127257686:AAEcZFgBiXQnkfL7BG30L9gw_6Up9Thv8p0',
-  chat: '-1001844850616',
+  token: TELEGRAM_TOKEN,
+  chat: TELEGRAM_CHAT,
 };
 
 export const defaultUserAvatar = 'assets/avatars/default.svg';
